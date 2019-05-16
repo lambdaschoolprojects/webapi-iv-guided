5dd453203bb24a156508fd5fcb5fabe02103595e
RaneWallin <rane@ranewallin.com>
2019-05-15 19:03:54 -0700

    making PORT dynamic

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const server = require('./api/server.js');
+const PORT = process.env.PORT || 4000;
 
-server.listen(4000, () => {
-  console.log('\n*** Server Running on http://localhost:4000 ***\n');
+server.listen(PORT, () => {
+  console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
 });
